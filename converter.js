45c6bb74157b8f59569ba02f0f4d17d31cb3a7e2
Santeri Kasvi <santeri.kasvi@gmail.com>
2015-09-23 16:40:24 +0000

    Updated version

--- converter.js
+++ converter.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         RohBot Imperial to Metric
-// @version      1.0
+// @version      1.1
 // @description  Converts imperial to metric if it finds any
 // @author       Spans
 // @match        https://rohbot.net
